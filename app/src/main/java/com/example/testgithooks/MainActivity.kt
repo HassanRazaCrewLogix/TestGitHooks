d11faee33112b7817888518aca560b58baa23f24
[HassanRazaCrewLogix] <[hassan.raza@crewlogix.com>
2019-03-12 11:17:50 +0500

    Pre Commit

--- app/src/main/java/com/example/testgithooks/MainActivity.kt
+++ app/src/main/java/com/example/testgithooks/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        www eee rrr bbb
+        www eee rrr bbb bbbb bbbb
     }
 }
